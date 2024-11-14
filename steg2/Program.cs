b10cb33ec9dbd79230fcb0d7c053e1d7e558886f
jasmine lindberg <Jasmine.lindberg@elev.ga.ntig.se>
2024-11-14 13:05:38 +0100

    first commit

---
+++ steg2/Program.cs
@@ -0,0 +1,75 @@
+﻿// string[] leksaker =  ["barbie", "bratz", "kulor", "leksak", "chockladboll"];
+
+// foreach ( string i in leksaker)
+// {
+// Console.WriteLine(i);
+// }
+// List<string> name = ["Femboy1" , "Femboy2" , "mathias", "jasmine", "wing" ];
+
+// foreach( string i in name)
+// {
+// Console.WriteLine(i);
+// }
+
+// List <String> rate = ["2", "4", "6", "8", "10"];
+
+// foreach( string i in rate)
+// {
+// Console.WriteLine(i);
+// }
+
+// for (int i = 0; i < leksaker.Length; i++)
+// {
+
+// Console.WriteLine($" {name[i]} ger betyget {rate[i]} på {leksaker[i]}");
+
+// }
+
+string svar ; 
+
+List <string> cities = [];
+
+int slut = 0;
+
+
+while (slut == 0)
+svar = Console.ReadLine();
+{
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
+
+
+
+
